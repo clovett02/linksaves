3ac8da05eacb40048abdea2ac4796a4707d6ee84
chris <chris.lovett02@outlook.com>
2025-02-28 00:08:47 -0500

    Set version to 1.0.1
    Just for testing purposes with cicd and the python package repo.

--- Linksaves/setup.py
+++ Linksaves/setup.py
@@ -3,7 +3,7 @@ import pathlib
 
 setup(
     name="Linksaves",
-    version="1.0.0",
+    version="1.0.1",
     packages=find_packages(where="src"),
     install_requires=["mysql-connector-python>=9.2.0"],
     long_description="A simple script for creating symbolic links for gamesaves.",
