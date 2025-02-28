86d48f481f04b1bbd4e3c730bc5e6c7555b392d7
chris <chris.lovett02@outlook.com>
2025-02-28 00:54:29 -0500

    Import and use List from typing
    Some recent version of python aren't compatible with setting return
    types without it.

--- Linksaves/Save.py
+++ Linksaves/Save.py
@@ -1,4 +1,5 @@
 import mysql.connector as connector
+from typing import List
 
 
 class Save:
@@ -79,7 +80,7 @@ class Saves:
             user="linksavesScript",
             password="mchs2009")
         
-        self._saves: list[Save] = []
+        self._saves: List[Save] = []
         self.ExecuteQuery()
 
     @property
