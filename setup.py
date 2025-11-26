4b0bfcfe3fa2858bc7dd3ae7c29c75c0335811e9
Chris Lovett <chris.lovett02@outlook.com>
2025-11-26 04:15:47 -0500

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="Linksaves",
-    version="0.9.1",
+    version="0.9.2",
     packages=find_packages(),
     # packages=["."],
     install_requires=["mysql-connector-python>=9.2.0"],
