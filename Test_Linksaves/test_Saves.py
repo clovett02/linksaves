d2965cb21d2d661060a61b583ec2824ef7e2e089
chris <chris.lovett02@outlook.com>
2025-12-01 04:22:53 -0500

    updated tests for additions

--- Test_Linksaves/test_Saves.py
+++ Test_Linksaves/test_Saves.py
@@ -5,7 +5,7 @@ class test_Saves(unittest.TestCase):
     def test_SavesRetrieval(self):
         saves = Saves()
         self.assertGreater(len(saves.Saves), 3)
-
+        self.assertEqual(saves.Saves[1].Name, "Borderlands 2")
 
 if __name__ == "__main__":
     unittest.main()
